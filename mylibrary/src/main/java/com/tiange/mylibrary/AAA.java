436a9bb6ff1db8c6331cec97680edab63177a551
zdq <1208830279@qq.com>
2020-05-08 18:05:27 +0800

    第二次提交

---
+++ mylibrary/src/main/java/com/tiange/mylibrary/AAA.java
@@ -0,0 +1,16 @@
+package com.tiange.mylibrary;
+
+/**
+ * @author zdq
+ * 创建时间： 2020/5/8 16:04
+ */
+public class AAA {
+    public static int A = 1;
+
+    public static int B = 2;
+
+
+    int getSum() {
+        return (A + B);
+    }
+}
