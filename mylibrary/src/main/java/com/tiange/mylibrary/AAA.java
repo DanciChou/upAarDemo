4a72c68705837683b7945ff7f41e767999c5b577
zdq <1208830279@qq.com>
2020-05-09 09:45:39 +0800

    第三次提交

--- mylibrary/src/main/java/com/tiange/mylibrary/AAA.java
+++ mylibrary/src/main/java/com/tiange/mylibrary/AAA.java
@@ -1,5 +1,8 @@
 package com.tiange.mylibrary;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * @author zdq
  * 创建时间： 2020/5/8 16:04
@@ -10,7 +13,11 @@ public class AAA {
     public static int B = 2;
 
 
-    int getSum() {
+    public  int getSum() {
         return (A + B);
+    }
+
+    public void sumResult(Context context){
+        Toast.makeText(context,"A + B  =" +getSum(),Toast.LENGTH_SHORT).show();
     }
 }
